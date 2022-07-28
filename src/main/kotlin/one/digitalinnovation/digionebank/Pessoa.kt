4be10aa8ec5848e55a54276ac4eaae95f90c3cce
Thiago <thimarsilva18@gmail.com>
2022-07-28 10:21:25 -0300

    created person class

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    var nome: String = "Thiago"
+    var cpf: String = "123.456.789-10"
+}
+
+fun main () {
+    val thiago = Pessoa()
+
+    println(thiago.nome)
+    println(thiago.cpf )
+}
